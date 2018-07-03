fab2942073b99a20c5ed1b065d293233ecff3140
Jaewon Kwak <tsfo1489@gmail.com>
2018-07-03 10:24:24 +0900

    Fix 2167

--- C++/2167.cpp
+++ C++/2167.cpp
@@ -1,16 +1,20 @@
 #include<stdio.h>
-int map[305][305], d[305][305], n, m;
+int map[305][305], n, m;
 int main()
 {
-	int i, j;
+	int i, j, tmp;
 	scanf("%d %d", &n, &m);
-	for (i = 1; i <= n; i++) for (j = 1; j <= m; j++) scanf("%d", &map[i][j]);
-	for (i = 1; i <= n; i++)
+	for (i = 1; i <= n; i++) for (j = 1; j <= m; j++) 
 	{
-		for (j = 1; j <= m; j++)
-		{
-			d[i][j]=map[i][j]+d[i-1][j]
-		}
+		scanf("%d", &tmp);
+		map[i][j] = map[i-1][j] + map[i][j-1] - map[i-1][j-1] + tmp;
 	}
-
+	int T;
+	int x1,y1,x2,y2;
+	scanf("%d",&T);
+	while(T--)
+	{
+		scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+		printf("%d\n",map[x2][y2] - map[x1-1][y2] - map[x2][y1-1] + map[x1-1][y1-1]);
+	}
 }
